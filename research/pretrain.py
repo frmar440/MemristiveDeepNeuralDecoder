248904f9b070dabe04c7953be8c76afdd19c6294
frmar440 <frederic.marcotte.2@ulaval.ca>
2022-07-05 23:46:20 -0400

    pretrain iterative

--- research/pretrain.py
+++ research/pretrain.py
@@ -27,7 +27,36 @@ CONVERSION_MAP = {Linear: AnalogLinear,
                   Sequential: AnalogSequential,
                   DND: MDND}
 
-DATA_PATH = 'research/1QBit/test_data_d3/surfaceCodeRMX_d3_p01_Nt1M_rnnData_aT1651079378.txt'
+DATA_PATHS = [
+    'research/1QBit/test_data_d3/surfaceCodeRMX_d3_p0015_Nt1M_rnnData_aT1651078684.txt',
+    'research/1QBit/test_data_d3/surfaceCodeRMX_d3_p0035_Nt1M_rnnData_aT1651078734.txt',
+    'research/1QBit/test_data_d3/surfaceCodeRMX_d3_p005_Nt1M_rnnData_aT1651078773.txt',
+    'research/1QBit/test_data_d3/surfaceCodeRMX_d3_p006_Nt1M_rnnData_aT1651078797.txt',
+    'research/1QBit/test_data_d3/surfaceCodeRMX_d3_p0065_Nt1M_rnnData_aT1651078809.txt',
+    'research/1QBit/test_data_d3/surfaceCodeRMX_d3_p007_Nt1M_rnnData_aT1651078820.txt',
+    'research/1QBit/test_data_d3/surfaceCodeRMX_d3_p0085_Nt1M_rnnData_aT1651078854.txt',
+    'research/1QBit/test_data_d3/surfaceCodeRMX_d3_p01_Nt1M_rnnData_aT1651079378.txt'
+]
+IDS_DND = [
+    'fp_trained_dnd_model_d3_p0015_nU16_nR3',
+    'fp_trained_dnd_model_d3_p0035_nU16_nR3'
+    'fp_trained_dnd_model_d3_p005_nU16_nR3'
+    'fp_trained_dnd_model_d3_p006_nU16_nR3'
+    'fp_trained_dnd_model_d3_p0065_nU16_nR3'
+    'fp_trained_dnd_model_d3_p007_nU16_nR3'
+    'fp_trained_dnd_model_d3_p0085_nU16_nR3'
+    'fp_trained_dnd_model_d3_p01_nU16_nR3'
+]
+IDS_MDND = [
+    'fp_trained_mdnd_model_d3_p0015_nU16_nR3',
+    'fp_trained_mdnd_model_d3_p0035_nU16_nR3'
+    'fp_trained_mdnd_model_d3_p005_nU16_nR3'
+    'fp_trained_mdnd_model_d3_p006_nU16_nR3'
+    'fp_trained_mdnd_model_d3_p0065_nU16_nR3'
+    'fp_trained_mdnd_model_d3_p007_nU16_nR3'
+    'fp_trained_mdnd_model_d3_p0085_nU16_nR3'
+    'fp_trained_mdnd_model_d3_p01_nU16_nR3'
+]
 
 # model parameters
 INPUT_SIZE = 4
@@ -39,71 +68,67 @@ LEARNING_RATE = 1e-3
 BATCH_SIZE = 1024
 EPOCHS = 50
 
+for DATA_PATH, ID_DND, ID_MDND in zip(DATA_PATHS, IDS_DND, IDS_MDND):
+    # load training and test datasets
+    with open(DATA_PATH, 'rb') as f:
+        dico = pickle.loads(f.read())
 
-# load training and test datasets
-with open(DATA_PATH, 'rb') as f:
-    dico = pickle.loads(f.read())
+    training_decode_data = DecodeDataset(
+        dico=dico,
+        train=True,
+        transform=Lambda(lambda y: torch.tensor(y, dtype=torch.float)),
+        target_transform=Lambda(lambda y: torch.zeros(2, dtype=torch.float).scatter_(0, torch.tensor(y), value=1)) # one-hot encoding
+    )
 
-training_decode_data = DecodeDataset(
-    dico=dico,
-    train=True,
-    transform=Lambda(lambda y: torch.tensor(y, dtype=torch.float)),
-    target_transform=Lambda(lambda y: torch.zeros(2, dtype=torch.float).scatter_(0, torch.tensor(y), value=1)) # one-hot encoding
-)
-
-test_decode_data = DecodeDataset(
-    dico=dico,
-    train=False,
-    transform=Lambda(lambda y: torch.tensor(y, dtype=torch.float)),
-    target_transform=Lambda(lambda y: torch.zeros(2, dtype=torch.float).scatter_(0, torch.tensor(y), value=1)) # one-hot encoding
-)
-
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-# deep neural decoder
-model = DND(
-    input_size=INPUT_SIZE,
-    output_size=OUTPUT_SIZE,
-    hidden_size=HIDDEN_SIZE
-).to(device)
+    test_decode_data = DecodeDataset(
+        dico=dico,
+        train=False,
+        transform=Lambda(lambda y: torch.tensor(y, dtype=torch.float)),
+        target_transform=Lambda(lambda y: torch.zeros(2, dtype=torch.float).scatter_(0, torch.tensor(y), value=1)) # one-hot encoding
+    )
 
 
-# loss function
-loss_fn = torch.nn.CrossEntropyLoss()
-# optimizer
-optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
-# trainer
-trainer = Trainer(
-    training_data=training_decode_data,
-    test_data=test_decode_data,
-    learning_rate=LEARNING_RATE,
-    batch_size=BATCH_SIZE,
-    epochs=EPOCHS,
-    loss_fn=loss_fn,
-    optimizer=optimizer
-)
-# floating-point training
-trainer(model)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    # deep neural decoder
+    model = DND(
+        input_size=INPUT_SIZE,
+        output_size=OUTPUT_SIZE,
+        hidden_size=HIDDEN_SIZE
+    ).to(device)
 
 
-ID = f'fp_trained_dnd_model_d3_p01_nU16_nR3'
+    # loss function
+    loss_fn = torch.nn.CrossEntropyLoss()
+    # optimizer
+    optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
+    # trainer
+    trainer = Trainer(
+        training_data=training_decode_data,
+        test_data=test_decode_data,
+        learning_rate=LEARNING_RATE,
+        batch_size=BATCH_SIZE,
+        epochs=EPOCHS,
+        loss_fn=loss_fn,
+        optimizer=optimizer
+    )
+    # floating-point training
+    trainer(model)
 
-time = datetime.now()
-# save dnd
-torch.save(model.state_dict(), f'research/saves/dnd/{ID}-{time}.pth')
-# save fp training parameters
-with open(f'research/saves/dnd/{ID}-{time}.json', 'w') as file:
-    file.write(json.dumps(trainer.training_state_dict()))
+
+    time = datetime.now()
+    # save dnd
+    torch.save(model.state_dict(), f'research/saves/dnd/{ID_DND}-{time}.pth')
+    # save fp training parameters
+    with open(f'research/saves/dnd/{ID_DND}-{time}.json', 'w') as file:
+        file.write(json.dumps(trainer.training_state_dict()))
 
 
-ID = f'fp_trained_mdnd_model_d3_p01_nU16_nR3'
-
-# resistive processing unit
-rpu_config = InferenceRPUConfig()
-rpu_config.mapping = MappingParameter(digital_bias=False, # bias term is handled by the analog tile (crossbar)
-                                      max_input_size=512,
-                                      max_output_size=512)
-# convert dnd to mdnd
-analog_model = convert_to_analog(model, rpu_config, conversion_map=CONVERSION_MAP)
-# save mdnd
-torch.save(analog_model.state_dict(), f'research/saves/mdnd/{ID}-{time}.pth')
+    # resistive processing unit
+    rpu_config = InferenceRPUConfig()
+    rpu_config.mapping = MappingParameter(digital_bias=False, # bias term is handled by the analog tile (crossbar)
+                                        max_input_size=512,
+                                        max_output_size=512)
+    # convert dnd to mdnd
+    analog_model = convert_to_analog(model, rpu_config, conversion_map=CONVERSION_MAP)
+    # save mdnd
+    torch.save(analog_model.state_dict(), f'research/saves/mdnd/{ID_MDND}-{time}.pth')
